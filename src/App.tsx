4a75b45884c2b710c44906a4f54d2d92bdf5f517
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-02-08 16:37:23 -0500

    feat: adds news feed react A facebook knockoff. This commit includes a news feed page that loads mock data using the infinite scroll technique.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './styles.scss';
+import NewsFeed from "./pages/NewsFeed/NewsFeed";
+
+export default function App() {
+
+  return (
+    <NewsFeed />
+  );
+}
